b94c709eca518acb1f3e4189e3631089665a537d
utsavkafley <vastukafley@gmail.com>
2021-04-13 10:31:51 -0400

    Working on setting limit through GUI

--- RID.py
+++ RID.py
@@ -1,52 +1,57 @@
-import praw,requests,re
+import praw
+import requests
+import re
 import urllib.request
-import PySimpleGUI as sg      
+import PySimpleGUI as sg
 import os
 from tkinter import filedialog
 from tkinter import *
 
 
 ##########################GUI####################
+sg.theme('dark grey 9')
+layout = [[sg.Text('Please enter name of subreddit')],
+          [sg.InputText()],
+          [sg.Text('Download Limit (max 100)')],
+          [sg.In()],
+          [sg.Text('Path'),
+           sg.In(size=(25, 1), enable_events=False, key="-FOLDER-"),
+           sg.FolderBrowse()],
+          [sg.Submit(), sg.Cancel()]]
 
-layout = [[sg.Text('Please enter name of subreddit')],      
-                 [sg.InputText()],
-                 [sg.Text('Path')],
-                 [sg.In(size=(25,1), enable_events = True, key="-FOLDER-"), 
-                 sg.FolderBrowse()],      
-                 [sg.Submit(), sg.Cancel()]]      
+window = sg.Window('RedditImageDownloader', layout)
 
-window = sg.Window('RedditImageDownloader', layout)    
 
-event, values = window.read()    
+event, values = window.read()
 window.close()
 
-text_input = values[0]    
-
+text_input = values[0]
+size = values[1]
 
 ##################SCRIPT######################
 reddit = praw.Reddit(
     client_id="0-L0kz-b0jZ3Zw",
     client_secret="RLJy4IHTrGMPmuBANt56HPCztyhQEw",
-    username="Western_Animator_605",
-    password="BC5Drjr4E2XBNZ",
-    user_agent="web:1001-1 by(u/Western_Animator_605")
+    user_agent="linux:img_scraper:v0.1 (u/western_animator_605')")
 
 
 subreddit = reddit.subreddit(text_input)
-
-top = subreddit.top(limit = None)
+print(size)
+top = subreddit.top(limit=5)
 count = 0
 path = values["-FOLDER-"]
-for submission in top:
-    url = str(submission.url)
-    filepath = os.path.join("/home/utsav/Desktop/",f"image{count}")
-    # Check if the link is an image
-    if url.endswith("jpg") or url.endswith("jpeg") or url.endswith("png"):
+while True:
+    for submission in top:
+        url = str(submission.url)
+        filepath = os.path.join(path, f"image{count}")
+        # Check if the link is an image
+        if url.endswith("jpg") or url.endswith("jpeg") or url.endswith("png"):
 
-        # Retrieve the image and save it in current folder
-        urllib.request.urlretrieve(url, filepath)
-        count += 1
+            # Retrieve the image and save it in current folder
+            urllib.request.urlretrieve(url, filepath)
+            count += 1
 
-        # Stop once you have 10 images
-        if count == 10:
-            break
+            # Stop once you have input size of images
+            if count == size:
+                break
+
