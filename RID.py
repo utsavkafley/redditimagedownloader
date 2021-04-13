d175e416fd7687ff17a5b1cfed4b09a56b56fccd
utsavkafley <vastukafley@gmail.com>
2021-04-12 23:50:15 -0400

    Working script and simple GUI that takes subreddit as input

--- RID.py
+++ RID.py
@@ -1,8 +1,24 @@
 import praw,requests,re
 import urllib.request
+import PySimpleGUI as sg      
+import os
 
-from PIL import Image
 
+##########################GUI####################
+
+layout = [[sg.Text('Please enter name of subreddit')],      
+                 [sg.InputText()],      
+                 [sg.Submit(), sg.Cancel()]]      
+
+window = sg.Window('RedditImageDownloader', layout)    
+
+event, values = window.read()    
+window.close()
+
+text_input = values[0]    
+
+
+##################SCRIPT######################
 reddit = praw.Reddit(
     client_id="0-L0kz-b0jZ3Zw",
     client_secret="RLJy4IHTrGMPmuBANt56HPCztyhQEw",
@@ -11,24 +27,21 @@ reddit = praw.Reddit(
     user_agent="web:1001-1 by(u/Western_Animator_605")
 
 
-subreddit = reddit.subreddit('earthporn')
+subreddit = reddit.subreddit(text_input)
 
 top = subreddit.top(limit = None)
 count = 0
 
 for submission in top:
     url = str(submission.url)
-
+    filepath = os.path.join("/home/utsav/Desktop/",f"image{count}")
     # Check if the link is an image
     if url.endswith("jpg") or url.endswith("jpeg") or url.endswith("png"):
 
         # Retrieve the image and save it in current folder
-        urllib.request.urlretrieve(url, f"image{count}")
+        urllib.request.urlretrieve(url, filepath)
         count += 1
 
         # Stop once you have 10 images
         if count == 10:
             break
-
-
-
